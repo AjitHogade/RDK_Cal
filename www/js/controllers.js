4fabb763c9004bbd18f0a36bf57975540cd8f1c4
ajit <aju.260890@gmail.com>
2015-11-27 01:28:31 +0530

    added fullCalemdar

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,12 +1,1 @@
 angular.module('ionicApp.controllers', [])
-
-.controller('NavCtrl', function($scope, $ionicSideMenuDelegate) {
-  $scope.showMenu = function () {
-    $ionicSideMenuDelegate.toggleLeft();
-  };
-  $scope.showRightMenu = function () {
-    $ionicSideMenuDelegate.toggleRight();
-  };
-})
-.controller('HomeTabCtrl', function($scope) {
-})
