e6b46625bf264c77d55f3216da3fbdfddf969ca2
ajit <aju.260890@gmail.com>
2015-12-13 23:50:31 +0530

    new events january month complete.

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -2,7 +2,7 @@ angular.module("ionicApp.controllers",[]);
 //---2016-01-01T07:00:00 /yyyy-mm-dd ------
 var events=[
   {
-     	title:' <img src="/dada.jpg" height="25" width="25">NEW YEAR <i class="fa fa-flag fa-2x" style="color:red"></i> <br>मकर-संक्रांत',
+     	title:' <img src="img/eventIco/dada.jpg" height="25" width="25">NEW YEAR <i class="fa fa-flag fa-2x" style="color:red"></i> <br>मकर-संक्रांत',
      	start:"2015-01-03",
      	description:'<i class="fa fa-flag" style="color:red"></i>long description<b style="color:red">sasasasa sasa </b><br>sasa sasa sasa sasas asa sasas asas as asa sas ',
         className:'publicHoliday',
@@ -53,7 +53,7 @@ var events=[
     {
 	title:"<i class='fa fa-circle fa-2x' style='color:black'></i> <br>अमावस्या",
 	start:"2016-01-09",
-    description:'<center><h2><i class="fa fa-circle fa-2x" style="color:black"></i>अमावस्या</h2></center><div class="allDetail">अमावस्या समाप्ती-सकाळी ०७.००<br>पौष शु.१<br>पूर्वाषाढा.०९.४१<br>पौष मासाआरंभ <br>ठाणे डवले नगर सप्ताह २</div>', 
+    description:'<center><h2><i class="fa fa-circle fa-2x" style="color:black"></i>अमावस्या</h2></center><div class="allDetail">दर्शवेळा अमावस्या प्रारंभ-सकाळी ०७.५३<br>मार्गशीर्ष कृ.१४<br>मार्गशीर्ष अमावस्या मूळ.१०.००<br>ठाणे डवले नगर सप्ताह प्रारंभ</div>', 
     },
     {
 	title:"पौष शु.१<br>पूर्वाषाढा.०९.४१",
@@ -61,104 +61,113 @@ var events=[
     description:'</h2></center><div class="allDetail">मार्गशीर्ष अमावस्या-मूळ.१०.००<br>दर्शवेळा अमावस्या प्रारंभ-सकाळी ०७.५३<br>मार्गशीर्ष कृ.१४<br>पौष मासाआरंभ <br>ठाणे डवले नगर सप्ताह २</div>', 
     },
     {
-	title:"Birthday Party",
 	start:"2016-01-11",
     description:'<center><h2>शिवरात्री</h2></center><div class="allDetail">मार्गशीर्ष कृ.१३<br> जेष्ठा०९.४७<br>शिवरात्री <br>श्री संत सेवागिरी महाराज <br>पुसेगाव रथ उत्सव </div>', 
     },
     {
-	title:"Birthday Party",
-	start:"2016-01-12",
-	imageurl:"img/dada.png"
-    },
-    {
-	title:"Birthday Party",
-	start:"2016-01-13",
-	imageurl:"img/dada.png"
-    },
-    {
-	title:"Birthday Party",
-	start:"2016-01-14",
-	imageurl:"img/dada.png"
-    },
-    {
-	title:"Birthday Party",
-	start:"2016-01-15",
-	imageurl:"img/dada.png"
-    },
-    {
-	title:"Birthday Party",
-	start:"2016-01-16",
-	imageurl:"img/dada.png"
-    },
-    {
-	title:"Birthday Party",
-	start:"2016-01-17",
-	imageurl:"img/dada.png"
-    },
-    {
-	title:"Birthday Party",
-	start:"2016-01-18",
-	imageurl:"img/dada.png"
-    },
-    {
-	title:"Birthday Party",
-	start:"2016-01-19",
-	imageurl:"img/dada.png"
-    },
-    {
-	title:"Birthday Party",
-	start:"2016-01-20",
-	imageurl:"img/dada.png"
-    },
-    {
-	title:"Birthday Party",
-	start:"2016-01-21",
-	imageurl:"img/dada.png"
-    },
-    {
-	title:"Birthday Party",
-	start:"2016-01-22",
-	imageurl:"img/dada.png"
-    },
-    {
-	title:"Birthday Party",
-	start:"2016-01-23",
-	imageurl:"img/dada.png"
-    },
-    {
-	title:"Birthday Party",
-	start:"2016-01-24",
-	imageurl:"img/dada.png"
-    },
-    {
-	title:"Birthday Party",
-	start:"2016-01-25",
-	imageurl:"img/dada.png"
-    },
-    {
-	title:"Birthday Party",
-	start:"2016-01-26",
-	imageurl:"img/dada.png"
-    },
-    {
-	title:"Birthday Party",
-	start:"2016-01-27",
-	imageurl:"img/dada.png"
-    },
-    {
-	title:"Birthday Party",
-	start:"2016-01-28",
-	imageurl:"img/dada.png"
-    },
-    {
-	title:"Birthday Party",
-	start:"2016-01-29",
-	imageurl:"img/dada.png"
-    },
-    {
-	title:"Birthday Party",
-	start:"2016-01-30",
-	imageurl:"img/dada.png"
-    }
+title:"पौष शु.२<br>उत्तराषाढा.०८.५७",
+start:"2016-01-11",
+description:"<center><h2>ठाणे डवले नगर सप्ताह ३</h2></center><div class='allDetail'>पौष शु.२<br>उत्तराषाढा.०८.५७<br>ठाणे डवले नगर सप्ताह ३</div>"
+},
+{
+title:"<img src='img/eventIco/vivekanand.jpg' height='25' width='25'><br>स्वामी विवेकानंद जयंती",
+start:"2016-01-12",
+description:"<center><h2><img src='img/eventIco/vivekanand.jpg' height='25' width='25'>स्वामी विवेकानंद जयंती</h2></center><div class='allDetail'>राष्ट्रीय युवा दिन <br>पौष शु.३<br>उत्तराषाढा.०७.५४<br>धनिष्ठा .३०.३९<br>ठाणे डवले नगर सप्ताह ४</div>"
+},
+{
+title:"शततारका .२९.१८<br>पौष शु.४",
+start:"2016-01-13",
+description:"<center><h2></h2></center><div class='allDetail'>पौष शु.४<br>शततारका .२९.१८<br>ठाणे डवले नगर सप्ताह ५</div>"
+},
+{
+title:"भोगी <br>धनुर्मास समाप्ती ",
+start:"2016-01-14",
+description:"<center><h2>भोगी </h2></center><div class='allDetail'>धनुर्मास समाप्ती<br>पूर्व भाद्रपदा.२७.५४<br> पौष शु.५<br>ठाणे डवले नगर सप्ताह ६</div>"
+},
+{
+title:"गुरुदेव दादांची शुध्द षष्ठी<br>मकर संक्रांती पुण्यकाल",
+start:"2016-01-15",
+description:"<center><h2>गुरुदेव दादांची शुध्द षष्ठी<br>मकर संक्रांती पुण्यकाल</h2></center><div class='allDetail'>मकर संक्रांती पुण्यकाल सूर्याउदयापासून - सुर्यास्तापर्यंत<br>श्रीगुरुभक्त मंडळ बोरखळ प्रसाद<br>ठाणे डवले नगर सप्ताह कला <br>उ.भाद्रपदा  २६.३२<br>पौष शु.६</div>"
+ },
+{
+title:"करि दिन <br>पौष शु.७",
+start:"2016-01-16",
+description:"<center><h2>करि दिन </h2></center><div class='allDetail'>पौष शु.७<br>रेवती .२५.१२</div>"
+ },
+{
+title:"<img src='img/eventIco/durga.jpg' height='25' width='25'><br>दुर्गाष्ठमी<br> पौष शु.८",
+start:"2016-01-17",
+description:"<center><h2><img src='img/eventIco/durga.jpg' height='25' width='25'>दुर्गाष्ठमी</h2></center><div class='allDetail'> पौष शु.८<br>अश्विनी .23.५७</div>"
+ },
+{
+title:" पौष शु.९<br>भरणी .२२.४७",
+start:"2016-01-18",
+description:"<center><h2></h2></center><div class='allDetail'> पौष शु.९<br>भरणी .२२.४७</div>"
+ },
+{
+title:" पौष शु.१०<br>कृतिका .२१.४५",
+start:"2016-01-19",
+description:"<center><h2></h2></center><div class='allDetail'>पौष शु.१०<br>कृतिका .२१.४५</div>"
+ },
+{
+title:" <i class='fa fa-flag fa-2x' style='color:red'></i> <br>पुत्रदा एकादशी",
+start:"2016-01-20",
+description:"<center><h2> <i class='fa fa-flag fa-4x' style='color:red'></i> पुत्रदा एकादशी</h2></center><div class='allDetail'>पौष शु.११<br>रोहिणी.२०.५३</div>"
+ },
+{
+title:"मृगशीर्ष .२०.१७<br>प्रदोष",
+start:"2016-01-21",
+description:"<center><h2>प्रदोष</h2></center><div class='allDetail'>मृगशीर्ष .२०.१७<br>प्रदोष</div>"
+ },
+{
+title:"पौष शु.१३<br>आद्रा .१९.५९",
+start:"2016-01-22",
+description:"<center><h2></h2></center><div class='allDetail'>पौष शु.१३<br>आद्रा .१९.५९</div>"
+ },
+{
+title:"<i class='fa fa-circle-o fa-2x' style='color:'></i><br>पौष पोर्णिमा ",
+start:"2016-01-23",
+description:"<center><h2>नेताजी सुभाषचंद्र बोस जयंती</h2></center><div class='allDetail'><br>शाकंभरी पोर्णिमा प्रारंभ - सकाळी ०७.३०<br>पौष शु.१४/ पौष पोर्णिमा<br>पुनर्वसु .२०.०६</div>"
+ },
+{
+title:"पौष शु.१<br>पोर्णिमा समाप्ती सकाळी ०७.१५",
+start:"2016-01-24",
+description:"<center><h2></h2></center><div class='allDetail'>पोर्णिमा समाप्ती सकाळी ०७.१५<br>पौष शु.१<br>पुष्य .२०.४३</div>"
+ },
+{
+title:"पौष शु.१<br>आश्लेषा .१९.५९",
+start:"2016-01-25",
+description:"<center><h2></h2></center><div class='allDetail'>पौष शु.१<br>आश्लेषा .१९.५९</div>"
+ },
+{
+title:"<img src='img/eventIco/flag.jpg' height='25' width='25'><br>गणराज्य दिन ",
+start:"2016-01-26",
+description:"<center><h2><img src='img/eventIco/flag.jpg' height='25' width='25'><br>गणराज्य दिन </h2></center><div class='allDetail'>पौष शु.२<br>मघा .२३.३८<br></div>"
+ },
+{
+title:"<img src='img/eventIco/ganesh.jpg' height='25' width='25'><br>संकष्ट चतुर्थी",
+start:"2016-01-27",
+description:"<center><h2><img src='ganesh.jpg' height='25' width='25'><br>संकष्ट चतुर्थी</h2></center><div class='allDetail'>चंद्रोदय ०९.२२<br>पूर्वा .२५.५७<br>पौष कृ.३</div>"
+ },
+{
+title:"उत्तरा .२८.४२<br>पौष कृ.४",
+start:"2016-01-28",
+description:"<center><h2></h2></center><div class='allDetail'> पौष कृ.४<br>उत्तरा .२८.४२</div>"
+ },
+{
+title:"हस्त .अहोरात्र<br> पौष कृ.५",
+start:"2016-01-29",
+description:"<center><h2></h2></center><div class='allDetail'>हस्त .अहोरात्र<br> पौष कृ.५</div>"
+ },
+{
+title:"<img src='img/eventIco/gandhi.jpg' height='25' width='25'><br>महात्मा गांधी पुण्यतिथी",
+start:"2016-01-30",
+description:"<center><h2><img src='img/eventIco/gandhi.jpg' height='25' width='25'>महात्मा गांधी पुण्यतिथी</h2></center><div class='allDetail'>हुतात्मा दिन <br> पौष कृ.६<br>हस्त .०७.४४</div>"
+ },
+{
+title:"भानुसप्तमी",
+start:"2016-01-30",
+description:"<center><h2></h2></center><div class='allDetail'>भानुसप्तमी<br>पौष कृ.७<br>चित्रा .१०.५०</div>"
+ }
 
      ];
