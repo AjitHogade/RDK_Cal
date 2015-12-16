7e50162b5a3dd9d414c3a4991e9cc081a3a356e6
Ajit <aju.20890@gmail.com>
2015-12-16 15:41:43 +0530

    multiple albums selection bug fixed.

--- www/js/app.js
+++ www/js/app.js
@@ -232,12 +232,25 @@ $scope.safeApply = function(fn) {
     src: 'img/gallery/alandi_saptah/Image10.jpg'
   }
   ];
+    $scope.newImages = [
+  {
+    src: 'img/pic1.jpg'
+  }, {
+    src: 'img/pic2.jpg'
+  }, {
+    src: 'img/pic3.jpg'
+  }
+  ];
 
   $scope.zoomMin = 1;
 
   $scope.showImages = function(index) {
     $scope.activeSlide = index;
     $scope.showModal('templates/gallery-zoomview.html');
+  };
+$scope.Images = function(index1) {
+    $scope.activeSlide = index1;
+    $scope.showModal('templates/gallery2.html');
   };
 
   $scope.showModal = function(templateUrl) {
